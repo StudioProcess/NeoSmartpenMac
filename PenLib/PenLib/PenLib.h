c811fe1a996953b01ffcaf04a849ab2a4929bcd9
Martin Leopold <m@martinleopold.com>
2017-11-12 08:46:23 +0100

    Initial setup
    Minimal port of SDK (Objective-C, static lib) and bare bones sample
    (Swift).
    Connecting to pen works, getting no stroke data though.

---
+++ PenLib/PenLib/PenLib.h
@@ -0,0 +1,15 @@
+//
+//  PenLib.h
+//  PenLib
+//
+//  Created by Martin Leopold on 10.11.17.
+//  Copyright © 2017 Process. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NJPenCommManager.h"
+#import "NJNode.h"
+
+@interface PenLib : NSObject
+
+@end
